2d51d9638d321c66a0a61e917505024aa47eced7
Markus Knapp <github@direcs.de>
2018-09-08 10:17:26 +0200

    Fixing missing curly brackets and indentation

--- demo/piTest.c
+++ demo/piTest.c
@@ -541,70 +541,95 @@ void writeData(int offset, int length, unsigned long i32uValue)
  ************************************************************************************/
 void writeVariableValue(char *pszVariableName, uint32_t i32uValue)
 {
-    int rc;
-    SPIVariable sPiVariable;
-    SPIValue sPIValue;
-    uint8_t i8uValue;
-    uint16_t i16uValue;
+  int rc;
+  SPIVariable sPiVariable;
+  SPIValue sPIValue;
+  uint8_t i8uValue;
+  uint16_t i16uValue;
 
-    strncpy(sPiVariable.strVarName, pszVariableName, sizeof(sPiVariable.strVarName));
-    rc = piControlGetVariableInfo(&sPiVariable);
-    if (rc < 0) {
-      // cgi-bin compatible output...
-      printf("Content-Type: text/html;charset=utf-8");
-	    printf("Cannot find variable '%s'\n", pszVariableName);
-	return;
-    }
-
-    if (sPiVariable.i16uLength == 1) {
-	sPIValue.i16uAddress = sPiVariable.i16uAddress;
-	sPIValue.i8uBit = sPiVariable.i8uBit;
-	sPIValue.i8uValue = i32uValue;
-	rc = piControlSetBitValue(&sPIValue);
-	if (rc < 0)
-      // cgi-bin compatible output...
-      printf("Content-Type: text/html;charset=utf-8");
-	    printf("Set bit error %s\n", getWriteError(rc));
-	else
+  strncpy(sPiVariable.strVarName, pszVariableName, sizeof(sPiVariable.strVarName));
+  rc = piControlGetVariableInfo(&sPiVariable);
+  if (rc < 0)
   {
+    // cgi-bin compatible output...
+    printf("Content-Type: text/html;charset=utf-8");
+    printf("Cannot find variable '%s'\n", pszVariableName);
+    return;
+  }
+
+  if (sPiVariable.i16uLength == 1)
+  {
+    sPIValue.i16uAddress = sPiVariable.i16uAddress;
+    sPIValue.i8uBit = sPiVariable.i8uBit;
+    sPIValue.i8uValue = i32uValue;
+    rc = piControlSetBitValue(&sPIValue);
+    if (rc < 0)
+    // cgi-bin compatible output...
+    printf("Content-Type: text/html;charset=utf-8");
+    printf("Set bit error %s\n", getWriteError(rc));
+    else
+    {
       printf("Content-Type: text/html;charset=utf-8");
-	    printf("Set bit %d on byte at offset %d. Value %d\n", sPIValue.i8uBit, sPIValue.i16uAddress,
-		   sPIValue.i8uValue);
-     }
-    } else if (sPiVariable.i16uLength == 8) {
-	i8uValue = i32uValue;
-	rc = piControlWrite(sPiVariable.i16uAddress, 1, (uint8_t *) & i8uValue);
-	if (rc < 0)
-      // cgi-bin compatible output...
-      printf("Content-Type: text/html;charset=utf-8");
-	    printf("Write error %s\n", getWriteError(rc));
-	else
-      // cgi-bin compatible output...
-      printf("Content-Type: text/html;charset=utf-8");
-	    printf("Write value %d dez (=%02x hex) to offset %d.\n", i8uValue, i8uValue,
-		   sPiVariable.i16uAddress);
-    } else if (sPiVariable.i16uLength == 16) {
-	i16uValue = i32uValue;
-	rc = piControlWrite(sPiVariable.i16uAddress, 2, (uint8_t *) & i16uValue);
-	if (rc < 0)
-       // cgi-bin compatible output...
-      printf("Content-Type: text/html;charset=utf-8");
-	    printf("Write error %s\n", getWriteError(rc));
-	else
-	    printf("Write value %d dez (=%04x hex) to offset %d.\n", i16uValue, i16uValue,
-		   sPiVariable.i16uAddress);
-    } else if (sPiVariable.i16uLength == 32) {
-	rc = piControlWrite(sPiVariable.i16uAddress, 4, (uint8_t *) & i32uValue);
-	if (rc < 0)
-      // cgi-bin compatible output...
-      printf("Content-Type: text/html;charset=utf-8");
-	    printf("Write error %s\n", getWriteError(rc));
-	else
-      // cgi-bin compatible output...
-      printf("Content-Type: text/html;charset=utf-8");
-	    printf("Write value %d dez (=%08x hex) to offset %d.\n", i32uValue, i32uValue,
-		   sPiVariable.i16uAddress);
+      printf("Set bit %d on byte at offset %d. Value %d\n", sPIValue.i8uBit, sPIValue.i16uAddress,
+      sPIValue.i8uValue);
     }
+  }
+  else
+  {
+    if (sPiVariable.i16uLength == 8)
+    {
+      i8uValue = i32uValue;
+      rc = piControlWrite(sPiVariable.i16uAddress, 1, (uint8_t *) & i8uValue);
+      if (rc < 0)
+      // cgi-bin compatible output...
+      printf("Content-Type: text/html;charset=utf-8");
+      printf("Write error %s\n", getWriteError(rc));
+      else
+      // cgi-bin compatible output...
+      printf("Content-Type: text/html;charset=utf-8");
+      printf("Write value %d dez (=%02x hex) to offset %d.\n", i8uValue, i8uValue,
+      sPiVariable.i16uAddress);
+    }
+    else
+    {
+      if (sPiVariable.i16uLength == 16)
+      {
+        i16uValue = i32uValue;
+        rc = piControlWrite(sPiVariable.i16uAddress, 2, (uint8_t *) & i16uValue);
+        if (rc < 0)
+        {
+          // cgi-bin compatible output...
+          printf("Content-Type: text/html;charset=utf-8");
+          printf("Write error %s\n", getWriteError(rc));
+        }
+        else
+        {
+          printf("Write value %d dez (=%04x hex) to offset %d.\n", i16uValue, i16uValue,
+          sPiVariable.i16uAddress);
+        }
+      }
+      else
+      {
+        if (sPiVariable.i16uLength == 32)
+        {
+          rc = piControlWrite(sPiVariable.i16uAddress, 4, (uint8_t *) & i32uValue);
+          if (rc < 0)
+          {
+            // cgi-bin compatible output...
+            printf("Content-Type: text/html;charset=utf-8");
+            printf("Write error %s\n", getWriteError(rc));
+          }
+          else
+          {
+            // cgi-bin compatible output...
+            printf("Content-Type: text/html;charset=utf-8");
+            printf("Write value %d dez (=%08x hex) to offset %d.\n", i32uValue, i32uValue,
+            sPiVariable.i16uAddress);
+          }
+        }
+      }
+    }
+  }
 }
 
 /***********************************************************************************/
